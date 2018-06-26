c1573b4042e7071679bfb922975d1e7965216f43
Nathan Alvarez <nathan21097@yahoo.com>
2018-06-26 02:12:26 -0500

    Appeal Document WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ScreenLoginComponent } from './screen-login/screen-login.component';
 import { ScreenEvaluationFormComponent } from './screen-evaluation-form/screen-evaluation-form.component';
 import { ScreenPageNotFoundComponent } from './screen-page-not-found/screen-page-not-found.component';
+import { ScreenAppealFormComponent } from './screen-appeal-form/screen-appeal-form.component';
 
 const appRoutes: Routes = [
     { path: 'login', 
         component: ScreenLoginComponent },
     { path: 'evaluation',
         component: ScreenEvaluationFormComponent,
+    },
+    { path: 'appeal',
+        component: ScreenAppealFormComponent,
     },
     { path: '',
     redirectTo: '/login',
@@ -25,7 +29,8 @@ const appRoutes: Routes = [
         AppComponent,
         ScreenLoginComponent,
         ScreenEvaluationFormComponent,
-        ScreenPageNotFoundComponent
+        ScreenPageNotFoundComponent,
+        ScreenAppealFormComponent
     ],
   imports: [
       RouterModule.forRoot(
---
+++ src/app/screen-appeal-form/screen-appeal-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screen-appeal-form',
+  templateUrl: './screen-appeal-form.component.html',
+  styleUrls: ['./screen-appeal-form.component.css']
+})
+export class ScreenAppealFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
