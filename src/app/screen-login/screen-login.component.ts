61016459a2a7aa481e18437199fc9c9a52d2d102
Kyle Adams <kyleadams3000@hotmail.com>
2018-06-26 13:44:46 -0500

    Changes

--- src/app/screen-login/screen-login.component.ts
+++ src/app/screen-login/screen-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-screen-login',
@@ -10,8 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScreenLoginComponent implements OnInit {
 
-  constructor() { }
+    protected router : Router;
+
+  constructor(router : Router) { 
+      this.router = router;
+  }
 
   ngOnInit() {
+  }
+
+  buttonLogin() : void {
+      this.router.navigateByUrl('/evaluation');
   }
 }
