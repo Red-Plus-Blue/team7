26c1981f5f13cc5dcc3ca771ddcb19c38c307952
Kyle Adams <kyleadams3000@hotmail.com>
2018-06-28 13:03:50 -0500

    Moved some styles to css, linked bootstrap css, minor reformatting

--- src/app/screen-appeal-form/screen-appeal-form.component.ts
+++ src/app/screen-appeal-form/screen-appeal-form.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-screen-appeal-form',
   templateUrl: './screen-appeal-form.component.html',
-  styleUrls: ['./screen-appeal-form.component.css']
+  styleUrls: [
+      './screen-appeal-form.component.css',
+      '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+    ]
 })
 export class ScreenAppealFormComponent implements OnInit {
 
