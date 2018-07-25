3732594d0262735e0e5be7bab18a9a6665352caa
Nathan Alvarez <nathan21097@yahoo.com>
2018-07-25 13:45:31 -0500

    Working Add and Delete Button on Pay form WIP

--- src/app/screen-evaluation-pay-form/screen-evaluation-pay-form.component.ts
+++ src/app/screen-evaluation-pay-form/screen-evaluation-pay-form.component.ts
@@ -10,36 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ScreenEvaluationPayFormComponent implements OnInit {
-  num=1;
-  entryd= new entry(1,1,'','';)
-  ENTRYS=[new entry(this.num,1,'file1','file2')];
-  addEntry(){
-    this.num+=1;
-    const newEntry=new entry(this.num,this.entryd.rating,this.entryd.goals,this.entryd.summary);
-    this.ENTRYS.push(newEntry);
-    this.resetEntrys();
-  }
-  deleteEntry(num:number){
-      for (let i = this.ENTRYS.length - 1; i &amp;amp;amp;gt;= 0; i--) {
-        if (this.ENTRYS[i].num === num) {
-          this.ENTRYS.splice(i, 1);
-        }
-      }
-  }
-  resetTeacher() {
-     this.entry.rating = 1;
-     this.entry.goals = '';
-     this.entry.summary = '';
-    }
  	constructor() { }
 
   	ngOnInit() {
   	}   
-  class entry{
-  constructor(
-    public num:number,
-    public rating:number,
-    public goals:string,
-    public summary:string){ }
-} 
+    private entArray: Array<any> = [];
+    private newentry: any = {};
+    addEntry() {
+        this.entArray.push(this.newentry)
+        this.newentry = {};
+    }
+
+    deleteEntry(index) {
+        this.entArray.splice(index, 1);
+    }
 }
